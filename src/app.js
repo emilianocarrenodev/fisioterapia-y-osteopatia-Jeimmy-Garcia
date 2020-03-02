f6c209bffe8bb256f18b45493784096df192e8ae
Emiliano Carreño <emiliano0662@gmail.com>
2020-03-01 22:31:41 -0500

    avances home

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 import './scss/style.scss';
 import 'bootstrap';
 import { WOW } from 'wowjs';
-import { Swiper, Pagination, Navigation } from 'swiper/js/swiper.esm.js';
-
-Swiper.use([Pagination, Navigation]);
-
-var page = $('body').data('page');
+import { Swiper } from 'swiper/js/swiper.esm.js';
 
 var wow = new WOW({
     live: false,
@@ -21,15 +17,6 @@ if ($.contains(document.body, document.getElementById('slider-main'))) {
     var mySwiper = new Swiper('.swiper-container', {
         autoplay: {
             delay: 5000,
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-            clickable: true
-        },
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
         },
         loop: true
     });
